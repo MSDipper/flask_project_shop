81e971c983ca7a27b65744f76e0807b90fe74b00
MSDipper <github5248@gmail.com>
2023-02-10 23:58:27 +0300

    rename app

--- posts/posts.py
+++ posts/posts.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from models import Product
 
 
 posts = Blueprint("posts", __name__, template_folder='templates', static_folder='static')
 
 @posts.route('/')
 def home():
-    return render_template('posts/store.html')
+    posts = Product.query.all()
+    return render_template('home.html', posts=posts)
