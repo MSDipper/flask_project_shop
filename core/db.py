f2b1b750d127e879bdb3411cd2cde3cce5f83dcd
MSDipper <102433692+MSDipper@users.noreply.github.com>
2023-02-13 00:06:44 +0300

    Delete core directory

--- core/db.py
+++
@@ -1,6 +0,0 @@
-from app import app
-from flask_sqlalchemy import SQLAlchemy
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:11@localhost/electro_shop_flask"
-db = SQLAlchemy(app)
