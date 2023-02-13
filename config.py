1b4bbe94639f6bc34e85dc998557676d48165b0a
MSDipper <102433692+MSDipper@users.noreply.github.com>
2023-02-13 15:29:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,3 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:11@localhost/electro_shop_flask"
     SECRET_KEY = 'ddd53ed4f245501895d4f0e113da9587355fdf3e3fdb7a2ac5b87f667e679b5dcb2888'
-    # <!-- {% for product, category in products %} -->
